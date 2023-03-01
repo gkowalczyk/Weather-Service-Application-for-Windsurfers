a942a01a514ea49c7c092773796d1f5268206987
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2023-03-01 14:02:31 +0100

    first commit

---
+++ src/main/java/com/gkowalczyk/weatherserviceforwindsurfers/WeatherServiceForWindSurfersApplication.java
@@ -0,0 +1,13 @@
+package com.gkowalczyk.weatherserviceforwindsurfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceForWindSurfersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherServiceForWindSurfersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gkowalczyk/weatherserviceforwindsurfers/WeatherServiceForWindSurfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkowalczyk.weatherserviceforwindsurfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceForWindSurfersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
